ff294aac04ef0d0b71f51a81e49bc8a588651bb9
Robert <robert@qubitdigital.com>
2015-01-29 14:31:54 +0000

    Mmmm donuts

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# uv-api
 Universal Variable API
+----------------------
+
+_The Universal Variable API moves business level data into another dimension by introducing a completely event based approach to representing page level data flow._
+
+This project contains the source code and tests for the Universal Variable API. A script tag with a minified version of the API should be inserted inline for all html documents, at the top of head, immediately after the `<meta charset... />` tag.
+
+Methodology
+===========
+
+Universal Variable is used for exposing internal data to third party businesses integrating technology with websites and apps. Until recently it has sufficed to provide a global object with an ecommerce-based schema however as web and mobile apps become more prominant it has become difficult to represent data changes as the visitor navigates the app with few or no page reloads. Furthermore the current schema is very restrictive, only allowing a small part of the backend data to be represnted in a common format.
+
+
+Moving forward all data is exposed as events using a global API. There is a range of event schemas to represent data for many different use cases, namespacing is used to group events that are related. Event schemas contain nested objects that also have schema. Note that the schemas have been built so that events carry a lot of relevant embedded information to avoid the need to join between events. Though there is a lot of replicated data in the schema, the embeddeding severly reduces lookup time and increases ease of use.
+
+Compatibility
+=============
+
+Tested in IE8+, Firefox, Opera, Chrome and Safari.
+
+API
+===
+
+### Emit
+
+`uv.emit(type, [data])`
+
+Emits an event with the __type__ and __data__ specified. The __data__ should conform to the schema for the event __type__ emitted.
+
+```javascript
+uv.emit('ec:product.view', {
+  product: {
+    id: '112-334-a',
+    price: 6.99,
+    name: '18th Birthday Baloon',
+    breadcrumb: ['Party Accessories', 'Birthday Parties']
+  },
+  color: 'red',
+  stock: 6
+});
+// => emits an ec:product.view event
+```
+
+### On
+
+`uv.on(type, handler, [context])`
+
+Attaches an event __handler__ to be called when a certain event __type__ is emitted. The __handler__ will be passed the event type and event data as arguments and will be bound to the __context__ object, if one is passed. Returns a subscription object which can detatch the handler using the dispose method. If an event __type__ `*` is passed, the handler will execute on all events.
+
+```javascript
+uv.on('ec:product.view', function (type, data) {
+  console.log(type);
+  console.log(data);
+});
+// => logs type and data when an `ec:product.view` event is emitted
+var sub = uv.on('*', function (type, data) {
+  console.log(type);
+  console.log(data);
+});
+// => logs type and data for all events
+sub.dispose();
+// => detatches the event handler
+```
+
+
+### Once
+
+`uv.once(type, handler, [context])`
+
+Attaches an event __handler__ that will be called once, only on the next event emitted that matches the __type__ specified. The __handler__ will be passed the event type and event data as arguments and will be bound to the context object, if one is passed. Returns a subscription object which can detatch the __handler__ using the dispose method. If an event __type__ `*` is passed, the __handler__ will execute on the next event regardless of type.
+
+
+```javascript
+uv.once('ec:product.view', function (type, data) {
+  console.log(type);
+  console.log(data);
+});
+emit('ec:product.view');
+// => logs type and data
+emit('ec:product.view');
+// => does not log
+```
+
+
+### Events
+
+The events array is a cache of events emitted since the last page load. By iterating over the array it is possible to interpret the user journey or the current state of the page.
---
+++ test/test-uv-api.js
@@ -0,0 +1,265 @@
+describe('Universal Variable API', function () {
+  var uv;
+
+  beforeEach(function () {
+    uv = window.uv;
+  });
+
+  afterEach(function () {
+    uv.listeners.length = 0;
+    uv.events.length = 0;
+  });
+
+  it('should create a window object', function () {
+    expect(window.uv).to.not.be(undefined);
+  });
+  it('should expose emit, on, once and reset methods and event and listener arrays only', function () {
+    expect(window.uv).to.only.have.keys('events', 'listeners', 'emit', 'on', 'once', 'reset');
+  });
+
+  describe('emit', function () {
+    var t1, t2, eventsLength;
+
+    beforeEach(function () {
+      uv.on('ec:transaction', function () {
+        eventsLength = uv.events.length;
+      });
+      t1 = new Date();
+      uv.emit('search');
+      uv.emit('ec:product.view');
+      uv.emit('search');
+      uv.emit('ec:product.view', {
+        stock: 14
+      });
+      uv.emit('ec:basket.add');
+      uv.emit('ec:transaction');
+      t2 = new Date();
+    });
+
+    it('should add each event to the events array', function () {
+      expect(uv.events.length).to.be(6);
+    });
+    it('should create a meta property with cts, cid and type properties', function () {
+      forEach(uv.events, function (event) {
+        expect(event.meta).to.only.have.keys('cts', 'cid', 'type');
+      });
+    });
+    it('should keep data associated with the event', function () {
+      expect(uv.events[3]).to.only.have.keys('stock', 'meta');
+      expect(uv.events[3].stock).to.be(14);
+    });
+    it('should store events in the events array in order', function () {
+      expect(map(uv.events, function (event) {
+        return event.meta.type;
+      })).to.eql([
+        'search',
+        'ec:product.view',
+        'search',
+        'ec:product.view',
+        'ec:basket.add',
+        'ec:transaction'
+      ]);
+    });
+    it('should generate a unique ID', function () {
+      var ids = [];
+      forEach(uv.events, function (event) {
+        expect(event.meta.cid).to.be.a('string');
+        forEach(ids, function (id) {
+          expect(event.meta.cid).to.not.be(id);
+        });
+        ids.push(event.meta.cid);
+      });
+    });
+    it('should generate a timestamp', function () {
+      for (var i = 0; i < 6; i++) {
+        expect(uv.events[i].meta.cts >= t1.getTime()).to.be(true);
+        expect(uv.events[i].meta.cts <= t2.getTime()).to.be(true);
+      }
+    });
+    it('should add events to events array before calling listeners', function () {
+      expect(eventsLength).to.be(6);
+    });
+  });
+
+  describe('on listeners', function () {
+    var sub, searchEvents, allEvents, productEvents,
+      noneEvents, transactionEvents, errors, errorMemo,
+      allContext;
+
+    beforeEach(function () {
+      errors = [];
+      searchEvents = [];
+      allEvents = [];
+      productEvents = [];
+      noneEvents = [];
+      transactionEvents = [];
+      if (console && console.error) {
+        errorMemo = console.error;
+        console.error = function () {
+          errors.push(arguments);
+        };
+      }
+      uv.on('search', function () {
+        searchEvents.push(arguments);
+      });
+      uv.on('ec:product.view', function () {
+        throw new Error('Some listener error');
+      });
+      uv.on('ec:product.view', function () {
+        productEvents.push(arguments);
+      });
+      uv.on('*', function () {
+        allEvents.push(arguments);
+        allContext = this;
+      }, { hi: 'dude' });
+      uv.on('none', function () {
+        noneEvents.push(arguments);
+      });
+      sub = uv.on('ec:transaction', function () {
+        transactionEvents.push(arguments);
+      });
+      uv.emit('search', {
+        resultCount: 10
+      });
+      uv.emit('search', {
+        resultCount: 20
+      });
+      uv.emit('ec:product.view', {
+        id: 'a-fabulous-product'
+      });
+      uv.emit('ec:transaction', {
+        orderId: 'oh-so-orderlicious'
+      });
+      sub.dispose();
+      uv.emit('ec:transaction', {
+        orderId: 'oh-so-not-orderlicious'
+      });
+    });
+
+    afterEach(function () {
+      console.error = errorMemo;
+    });
+
+    it('should be called in the specified context', function () {
+      expect(allContext).to.eql({
+        hi: 'dude'
+      });
+    });
+    it('should listen to the specified type', function () {
+      expect(searchEvents.length).to.be(2);
+      forEach(searchEvents, function (args) {
+        expect(args[0].meta.type).to.be('search');
+      });
+      expect(searchEvents[0][0].resultCount).to.be(10);
+      expect(searchEvents[1][0].resultCount).to.be(20);
+    });
+    it('should not listen to events emitted with a different type', function () {
+      expect(noneEvents.length).to.be(0);
+    });
+    it('should listen to all events given a wildcard type', function () {
+      expect(allEvents.length).to.be(5);
+    });
+    it('should be called if another listener throws an error', function () {
+      expect(productEvents.length).to.be(1);
+    });
+    it('should throw an error if a listener throws an error', function () {
+      expect(errors.length).to.be(1);
+      expect(errors[0][0]).to.be('Error emitting UV event');
+      expect(errors[0][1].message).to.be('Some listener error');
+    });
+    it('should unsubscribe listeners if the dispose method is called', function () {
+      expect(transactionEvents.length).to.be(1);
+      expect(transactionEvents[0][0].orderId).to.be('oh-so-orderlicious');
+    });
+    it('should not throw an error if dispose is called twice', function () {
+      expect(sub.dispose).to.not.throwException();
+    });
+  });
+
+  describe('once listeners', function () {
+    var searchEvents, productEvents, searchSub, searchContext;
+
+    beforeEach(function () {
+      searchEvents = [];
+      productEvents = [];
+      searchSub = uv.once('search', function () {
+        searchEvents.push(arguments);
+        searchContext = this;
+      }, {
+        no: 'way'
+      });
+      var productSub = uv.once('ec:product.view', function () {
+        productEvents.push(arguments);
+      });
+      uv.emit('search', {
+        resultCount: 10
+      });
+      uv.emit('search', {
+        resultCount: 20
+      });
+      productSub.dispose();
+      uv.emit('ec:product.view', {
+        id: 'a-fabulous-product'
+      });
+    });
+
+    it('should be called in the specified context', function () {
+      expect(searchContext).to.eql({
+        no: 'way'
+      });
+    });
+    it('should only be called once', function () {
+      expect(searchEvents.length).to.be(1);
+    });
+    it('should not be called if dispose is called', function () {
+      expect(productEvents.length).to.be(0);
+    });
+    it('should not throw an error if dispose is called after the first call', function () {
+      expect(searchSub.dispose).to.not.throwException();
+    });
+  });
+
+  describe('reset', function () {
+    var listenerCalled, afterListenerCalled;
+
+    beforeEach(function () {
+      listenerCalled = false;
+      afterListenerCalled = false;
+      uv.emit('search');
+      uv.on('ec:product.view', function () {
+        listenerCalled = true;
+      });
+      uv.reset();
+      uv.emit('ec:product.view');
+      uv.on('search', function () {
+        afterListenerCalled = true;
+      });
+      uv.emit('search');
+      uv.reset();
+    });
+
+    it('should unsubscribe all listeners', function () {
+      expect(listenerCalled).to.be(false);
+    });
+    it('should continue to call listeners attached after', function () {
+      expect(afterListenerCalled).to.be(true);
+    });
+    it('should empty the events array', function () {
+      expect(uv.events.length).to.be(0);
+    });
+  });
+});
+
+function forEach(list, it) {
+  for (var i = 0; i < list.length; i++) {
+    it(list[i], i);
+  }
+}
+
+function map(list, it) {
+  var result = [];
+  forEach(list, function (val, i) {
+    result.push(it(val, i));
+  });
+  return result;
+}
