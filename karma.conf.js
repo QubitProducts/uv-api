4d1ccc77485b09f848b703320c01ba894bb99293
Robert Tod <roberttod@gmail.com>
2016-01-12 19:10:03 +0000

    Improve testing - Move more logic into each test so that they are easier to read - Use sinon - Update testing deps

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['mocha', 'expect'],
+    frameworks: ['mocha', 'expect', 'sinon'],
     files: [
       'uv-api.js',
       'test/test-uv-api.js'
